d1262d606e33b6065f1868fb03d3f83455db874f
Rangga <ranggaaldosas@gmail.com>
2023-12-10 22:05:22 +0700

    fix:fix config

--- script.sh
+++ script.sh
@@ -1,5 +1,12 @@
 sudo apt update
-sudo apt install python3-pip
-pip install fastapi
-pip install uvicorn
-pip install pymongo pydantic
+sudo apt install python3
+sudo apt install python3-pip -y
+sudo apt install python3.10-venv
+
+pip install flask
+pip install gunicorn
+pip install flask_pymongo
+pip install gevent
+
+# gunicorn -w 8 -k gevent app:app
+# source ~/fp-tka/test_rando/bin/activate
